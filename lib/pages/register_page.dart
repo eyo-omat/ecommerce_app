58dc12cd0645068cfb4fb423c3c44ac179cf19e6
Eyioyo Omatsola <eyo.omat@gmail.com>
2020-01-20 01:03:36 +0000

    Added regsiter form text inputs

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -2,15 +2,60 @@ import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
-
   RegisterPageState createState() => RegisterPageState();
-
 }
 
 class RegisterPageState extends State<RegisterPage> {
-  @override 
-
+  @override
   Widget build(BuildContext context) {
-    return Text("Hello World");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Register"),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Form(
+              child: Column(
+                children: <Widget>[
+                  Text('Register', style: Theme.of(context).textTheme.headline),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                          labelText: 'Username',
+                          hintText: 'Enter username, min length 6',
+                          icon: Icon(Icons.face, color: Colors.grey,)),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                          labelText: 'Email',
+                          hintText: 'Enter a valid email',
+                          icon: Icon(Icons.mail, color: Colors.grey,)),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                          labelText: 'Password',
+                          hintText: 'Enter password, min length 6',
+                          icon: Icon(Icons.lock, color: Colors.grey,)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
