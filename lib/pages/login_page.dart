d32dada74c03cfb5bdae0787cf14490b6d0181df
Eyioyo Omatsola <eyo.omat@gmail.com>
2020-01-20 23:58:25 +0000

    Created login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  LoginPageState createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _email, _password;
+
+  Widget _showTitile() {
+    return Text('Login', style: Theme.of(context).textTheme.headline);
+  }
+
+  Widget _showEmailInput() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0),
+      child: TextFormField(
+        validator: (val) => !val.contains('@') ? 'Invalid Email' : null,
+        onSaved: (val) => _email = val,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Email',
+            hintText: 'Enter a valid email',
+            icon: Icon(
+              Icons.mail,
+              color: Colors.grey,
+            )),
+      ),
+    );
+  }
+
+  Widget _showPasswordInput() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0),
+      child: TextFormField(
+        validator: (val) => val.length < 6 ? 'Password is too short' : null,
+        onSaved: (val) => _password = val,
+        obscureText: true,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Password',
+            hintText: 'Enter password, min length 6',
+            icon: Icon(
+              Icons.lock,
+              color: Colors.grey,
+            )),
+      ),
+    );
+  }
+
+  Widget _showFormActions() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0),
+      child: Column(
+        children: <Widget>[
+          RaisedButton(
+              child: Text(
+                'Submit',
+                style: Theme.of(context)
+                    .textTheme
+                    .body1
+                    .copyWith(color: Colors.black),
+              ),
+              elevation: 8.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
+              color: Theme.of(context).accentColor,
+              onPressed: () => _submit()),
+          FlatButton(
+            child: Text('New User? Register'),
+            onPressed: () => print('Register'),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _submit() {
+    final form = _formKey.currentState;
+
+    if (form.validate()) {
+      form.save();
+      print('Email: $_email, Password: $_password');
+    } else {
+      print('Form invalid');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  _showTitile(),
+                  _showEmailInput(),
+                  _showPasswordInput(),
+                  _showFormActions(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
