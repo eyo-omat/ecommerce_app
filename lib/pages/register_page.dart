3da731142df1ad0b135787b5f28eaaf3185c5b97
Eyioyo Omatsola <eyo.omat@gmail.com>
2020-01-20 00:33:28 +0000

    Added RegisterPage class

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+
+  RegisterPageState createState() => RegisterPageState();
+
+}
+
+class RegisterPageState extends State<RegisterPage> {
+  @override 
+
+  Widget build(BuildContext context) {
+    return Text("Hello World");
+  }
+}
