fcdfc9150acc777cbff22acc974db6b9dcbefbd9
Eyioyo Omatsola <eyo.omat@gmail.com>
2020-01-28 23:00:55 +0000

    Added AppBar to the products page

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -29,14 +29,35 @@ class ProductsPageState extends State<ProductsPage> {
   //   print(json.decode(storedUser));
 
   // }
+
+  final _appBar = PreferredSize(
+    preferredSize: Size.fromHeight(60.0),
+    child: StoreConnector<AppState, AppState>(
+      converter: (store) => store.state,
+      builder: (context, state){
+        return AppBar(
+          centerTitle: true,
+          title: SizedBox(child: state.user != null ? Text(state.user.username) : Text(''),),
+          leading: Icon(Icons.store),
+          actions: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(right: 12.0),
+              child: state.user != null ? IconButton(icon: Icon(Icons.exit_to_app), onPressed: () => print('pressed'),) : Text(''),
+            )
+          ],
+        );
+      },
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return StoreConnector<AppState, AppState>(
-      converter: (store) => store.state,
-      builder: (context, state) {
-        return state.user != null ? Text(state.user.username) : Text('');
-      },
-    );
+    return Scaffold(
+      appBar: _appBar,
+      body: Container(
+        child: Text('Products Page'),
+      ),
+      );
   }
 }
