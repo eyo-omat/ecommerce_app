32ee3ce3a6536edb21a52fc545562fd15cda7922
Eyioyo Omatsola <eyo.omat@gmail.com>
2020-02-01 12:22:14 +0000

    Added improvements for image display on products page using MediaQuery

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -67,7 +67,7 @@ class ProductsPageState extends State<ProductsPage> {
 
   @override
   Widget build(BuildContext context) {
-
+    final Orientation orientation = MediaQuery.of(context).orientation;
     return Scaffold(
       appBar: _appBar,
       body: Container(
@@ -83,7 +83,12 @@ class ProductsPageState extends State<ProductsPage> {
                     bottom: false,
                     child: GridView.builder(
                       itemCount: state.products.length,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: orientation == Orientation.portrait? 2:3, 
+                        mainAxisSpacing: 4.0, 
+                        crossAxisSpacing: 4.0,
+                        childAspectRatio: orientation == Orientation.portrait ? 1.0 : 1.3,
+                      ),
                       itemBuilder: (context, i) => ProductItem(item: state.products[i]),
                     ),
                   ),
