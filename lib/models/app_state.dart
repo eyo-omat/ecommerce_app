b5815e9e721266a04cfd7f78914a7546834f9752
Eyioyo Omatsola <eyo.omat@gmail.com>
2020-02-06 22:10:42 +0000

    Added card token to app state

--- lib/models/app_state.dart
+++ lib/models/app_state.dart
@@ -8,14 +8,16 @@ class AppState {
   final List<Product> products;
   final List<Product> cartProducts;
   final List<dynamic> cards;
+  final String cardToken;
 
-  AppState({@required this.user, @required this.products, @required this.cartProducts, @required this.cards});
+  AppState({@required this.user, @required this.products, @required this.cartProducts, @required this.cards, @required this.cardToken});
 
   factory AppState.initial() {
     return AppState(
       user: null,
       products: [],
-      cartProducts: []
+      cartProducts: [],
+      cardToken: ""
     );
   }
 }
