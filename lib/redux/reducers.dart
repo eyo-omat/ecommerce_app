faeb1ba5915f9865ca93515c31f6ee56913240da
Eyioyo Omatsola <eyo.omat@gmail.com>
2020-02-12 12:48:06 +0000

    Added an orders reducer

--- lib/redux/reducers.dart
+++ lib/redux/reducers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_ecommerce/models/app_state.dart';
+import 'package:flutter_ecommerce/models/order.dart';
 import 'package:flutter_ecommerce/models/product.dart';
 import 'package:flutter_ecommerce/models/user.dart';
 import 'package:flutter_ecommerce/redux/actions.dart';
@@ -9,9 +10,11 @@ AppState appReducer(AppState state, dynamic action) {
     products: productsReducer(state.products, action),
     cartProducts: cartProductsReducer(state.cartProducts, action),
     cards: cardsReducer(state.cards, action),
-    cardToken: cardTokenReducer(state.cardToken, action)
-  );
-}
+    cardToken: cardTokenReducer(state.cardToken, action),
+    orders: ordersReducer(state.orders, action)
+      );
+  }
+    
 
 User userReducer(User user, dynamic action){
   if (action is GetUserAction) {
@@ -58,4 +61,9 @@ String cardTokenReducer(String cardToken, dynamic action){
     return action.cardToken;
   }
    return cardToken;
+}
+
+List<Order> ordersReducer(List<Order> orders, dynamic action) {
+  //if (action is AddOrderAction)
+  return orders;
 }
