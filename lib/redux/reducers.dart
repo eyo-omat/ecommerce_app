99dc02c42890535c13aeb1092a2001179ab86947
Eyioyo Omatsola <eyo.omat@gmail.com>
2020-02-12 13:00:45 +0000

    Added the order reducer

--- lib/redux/reducers.dart
+++ lib/redux/reducers.dart
@@ -64,6 +64,8 @@ String cardTokenReducer(String cardToken, dynamic action){
 }
 
 List<Order> ordersReducer(List<Order> orders, dynamic action) {
-  //if (action is AddOrderAction)
+  if (action is AddOrderAction) {
+    return List.from(orders)..add(action.order);
+  }
   return orders;
 }
